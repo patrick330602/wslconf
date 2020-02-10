5ba68b4477d98d2b55875f61ab0350e6a1e4c6f6
Patrick Wu <wotingwu@live.com>
2020-02-11 01:19:38 +0800

    first script completed

---
+++ interop-on-wsl-side-benchmark.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Get 
+function cmd_version {
+    abranch=$((cd /mnt/c && cmd.exe /C "reg.exe query "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion" /v BuildBranch") | tail -n 2 | head -n 1 | sed -e 's|\r||g')
+    abranch=${abranch##* }
+    echo "$abranch"
+}
+
+function dexec_version {
+    bbranch=$(reg.exe query "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion" /v BuildBranch | tail -n 2 | head -n 1 | sed -e 's|\r||g')
+    bbranch=${bbranch##* }
+    echo "$bbranch"
+}
+
+function psh_version {
+    branch=$(powershell.exe -NonInteractive -NoProfile -Command "(Get-ItemProperty 'HKLM:\\Software\\Microsoft\\Windows NT\\CurrentVersion').'BuildBranch'" | cat)
+    echo "$branch"
+}
+
+echo "Getting the branch name of the current build from "
+echo "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\BuildBranch"
+echo
+echo "cmd.exe method:"
+time cmd_version
+echo
+echo "powershell method:"
+time psh_version
+echo
+echo "direct exec method:"
+time dexec_version
