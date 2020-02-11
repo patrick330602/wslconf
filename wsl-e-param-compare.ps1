b797bc540db7d845a69ed92f776f2c40a961aadd
Patrick Wu <wotingwu@live.com>
2020-02-11 16:53:31 +0800

    wsl.exe -e demo

---
+++ wsl-e-param-compare.ps1
@@ -0,0 +1,8 @@
+
+Write-Output "returned result of command"
+Write-Output "wsl.exe printenv SHELL:"
+wsl.exe printenv SHELL
+Write-Output ""
+Write-Output "returned result of command"
+Write-Output "wsl.exe -e printenv SHELL:"
+wsl.exe -e printenv SHELL
